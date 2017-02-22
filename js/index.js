1561b0dcb8ab0baeaae1635514353bed5bc45393
rvrvrv <ibanez19@sbcglobal.net>
2017-02-22 12:39:23 -0600

    Upload completed project files

---
+++ js/index.js
@@ -0,0 +1,66 @@
+/*jshint browser: true, esversion: 6*/
+/* global $, console */
+
+$(document).ready(function () {
+	//Set cache to false for new quotes
+	$.ajaxSetup({
+		cache: false
+	});
+	//Upon site loading, fetch a quote
+	fetchQuote();
+});
+
+function fetchQuote() {
+	//Animated exit
+	$('#quoteBtn').prop('disabled', true);
+	$('#quoteBtn').text('Getting quote...');
+	$('#theAuthor').addClass('fadeOutUp');
+	$('#theQuote').fadeTo(50, 0.005);
+	$('.bigBox').css('background', '#fef7a6');
+
+	//JSON/AJAX call
+	var p = $.getJSON('https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?')
+		.done((data) => {
+			checkQuote(data[0]); //Check for valid quote
+		})
+		.fail((err) => {
+			console.error('Error retrieving data. Trying again.');
+			fetchQuote();
+		});
+}
+
+function checkQuote(data) {
+	//Remove <p> tags and trim quote
+	let quote = data.content.slice(3, this.length - 5).trim();
+	//Store author data
+	let author = data.title;
+	//If quote/author is invalid, fetch new quote
+	if (quote.match(/[\[#<&;]|(shi|drun)/g) ||
+		author.match(/[#<&;\(]/g) ||
+		(quote + author).length > 135)
+		fetchQuote();
+	else setTimeout(() => {
+		console.log(quote);
+		displayQuote(quote, author);
+	}, 200);
+}
+
+function displayQuote(quote, author) {
+	//Display the quote and author
+	$('#theQuote').text(quote);
+	$('#theAuthor').html(`<i class="fa fa-ellipsis-v" style="padding-right: 2%;"></i>${author}`);
+	//Update Tweet link
+	$('#tweetLink').attr('href', `http://twitter.com/home?status="${quote}"%20-%20${author}`);
+	//Animated entrance  
+	$('.bigBox').css('background', '#c8f7d0');
+	$('#theQuote').fadeTo(200, 1);
+	$('#theAuthor').removeClass('fadeOutUp');
+	setTimeout(() => $('#theAuthor').addClass('fadeInDown'), 200);
+	$('#quoteBtn').prop('disabled', false);
+	$('#quoteBtn').text('Get another quote');
+}
+
+//When button is clicked, fetch a new quote
+$('#quoteBtn').click(() => {
+	fetchQuote();
+});
